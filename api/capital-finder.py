3ad84fa9d8b3cc934e13b351b36c5189072fba45
AnthonySinitsa <itonyemail@gmail.com>
2023-07-03 21:22:00 +0000

    usedCommand

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -25,22 +25,38 @@
 #     self.wfile.write(message.encode())
 #     return
 
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
 
+
+
+# from http.server import BaseHTTPRequestHandler
+# from urllib import parse
+
+
+# class handler(BaseHTTPRequestHandler):
+#   def do_GET(self):
+#     # parse the quey string
+#     path = self.path
+#     url_components = parse.urlsplit(path)
+#     query_string_list = parse.parse_qsl(url_components.query)
+#     dic = dict(query_string_list)
+
+#     name = dic.get("name")
+
+#     self.send_response(200)
+#     self.send_header("Content-type", "text/plain")
+#     self.end_headers()
+#     self.wfile.write(f"Howdy {name}".encode())
+#     return
+
+
+
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
-    # parse the quey string
-    path = self.path
-    url_components = parse.urlsplit(path)
-    query_string_list = parse.parse_qsl(url_components.query)
-    dic = dict(query_string_list)
-
-    name = dic.get("name")
-
     self.send_response(200)
-    self.send_header("Content-type", "text/plain")
+    self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(f"Howdy {name}".encode())
+    self.wfile.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode())
     return
