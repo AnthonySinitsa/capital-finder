bea40305e98a2c451f4bdbbcd81af06b07e8259e
AnthonySinitsa <itonyemail@gmail.com>
2023-07-03 21:56:37 +0000

    usedCommand

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
-from urllib import parse, requests
-# import requests
+from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
