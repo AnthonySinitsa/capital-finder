75a02749120e6caf85a51d7b55b98b59d7facea1
AnthonySinitsa <itonyemail@gmail.com>
2023-07-03 21:18:07 +0000

    usedCommand

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,27 +1,46 @@
+# from http.server import BaseHTTPRequestHandler
+# from urllib import parse, requests
+
+# class handler(BaseHTTPRequestHandler):
+#   def do_GET(self):
+#     path = self.path
+#     url_components = parse.urlsplit(path)
+#     query_string_list = parse.parse_qsl(url_components.query)
+#     dic = dict(query_string_list)
+    
+#     self.send_response(200)
+#     self.send_header('Content-type', 'text/plain')
+#     self.end_headers()
+    
+#     word = dic['word']
+#     url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+#     response = requests.get(url + word)
+#     data = response.json()
+#     definitions = []
+#     for word_data in data:
+#       definition = word_data['meanings'][0]['definitions'][0]['definition']
+#       definitions.append(definition)
+    
+#     message = str(definitions)
+#     self.wfile.write(message.encode())
+#     return
+
 from http.server import BaseHTTPRequestHandler
-from urllib import parse, requests
+from urllib import parse
+
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
+    # parse the quey string
     path = self.path
     url_components = parse.urlsplit(path)
     query_string_list = parse.parse_qsl(url_components.query)
     dic = dict(query_string_list)
-    
+
+    name = dic.get("name")
+
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header("Content-type", "text/plain")
     self.end_headers()
-    
-    word = dic['word']
-    url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-    response = requests.get(url + word)
-    data = response.json()
-    definitions = []
-    for word_data in data:
-      definition = word_data['meanings'][0]['definitions'][0]['definition']
-      definitions.append(definition)
-    
-    message = str(definitions)
-    self.wfile.write(message.encode())
+    self.wfile.write(f"Howdy {name}".encode())
     return
-  
